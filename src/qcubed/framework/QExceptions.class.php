30ad7e9cecec61883096da1216b968da9d347615
ratno putro sulistiyono <ratno@knoqdown.com>
2017-05-17 13:02:38 +0700

    qcubed/framework/QExceptions.class.php: menambahkan pengecekan class QCallerException, agar tidak collision

--- src/qcubed/framework/QExceptions.class.php
+++ src/qcubed/framework/QExceptions.class.php
@@ -1,120 +1,121 @@
 <?php
-
-/**
- * This is the main exception to be thrown by any
- * method to indicate that the CALLER is responsible for
- * causing the exception.  This works in conjunction with Qcodo's
- * error handling/reporting, so that the correct file/line-number is
- * displayed to the user.
- *
- * So for example, for a class that contains the method GetItemAtIndex($intIndex),
- * it is conceivable that the caller could call GetItemAtIndex(15), where 15 does not exist.
- * GetItemAtIndex would then thrown an IndexOutOfRangeException (which extends CallerException).
- * If the CallerException is not caught, then the Exception will be reported to the user.  The CALLER
- * (the script who CALLED GetItemAtIndex) would have that line highlighted as being responsible
- * for calling the error.
- *
- * The PHP default for exeception reporting would normally say that the "throw Exception" line in GetItemAtIndex
- * is responsible for throwing the exception.  While this is technically true, in reality, it was the line that
- * CALLED GetItemAtIndex which is responsible.  In short, this allows for much cleaner exception reporting.
- *
- * On a more in-depth note, in general, suppose a method OuterMethod takes in parameters, and ends up passing those
- * paremeters into ANOTHER method InnerMethod which could throw a CallerException.  OuterMethod is responsible
- * for catching and rethrowing the caller exception.  And when this is done, IncrementOffset() MUST be called on
- * the exception object, to indicate that OuterMethod's CALLER is responsible for the exception.
- *
- * So the code snippet to call InnerMethod by OuterMethod should look like:
- *    function OuterMethod($mixValue) {
- *        try {
- *            InnerMethod($mixValue);
- *        } catch (CallerException $objExc) {
- *            $objExc->IncrementOffset();
- *            throw $objExc;
- *        }
- *        // Do Other Stuff
- *    }
- * Again, this will assure the user that the line of code responsible for the excpetion is properly being reported
- * by the QCubed error reporting/handler.
- * @property-read int $Offset The exception offset.
- * @property-read string $BackTrace The exception backtrace.
- * @property-read string $TraceArray The exception backtrace in a form of an array.
- */
-class QCallerException extends Exception
-{
-
-    private $intOffset;
-    private $strTraceArray;
-
+if(!class_exists(QCallerException::class)) {
     /**
-     * The constructor of CallerExceptions.  Takes in a message string
-     * as well as an optional Offset parameter (defaults to 1).
-     * The Offset specificies how many calls up the call stack is responsible
-     * for the exception.  By definition, when a CallerException is called,
-     * at the very least the Caller of the most immediate function, which is
-     * 1 up the call stack, is responsible.  So therefore, by default, intOffset
-     * is set to 1.
+     * This is the main exception to be thrown by any
+     * method to indicate that the CALLER is responsible for
+     * causing the exception.  This works in conjunction with Qcodo's
+     * error handling/reporting, so that the correct file/line-number is
+     * displayed to the user.
      *
-     * It is rare for intOffset to be set to an integer other than 1.
+     * So for example, for a class that contains the method GetItemAtIndex($intIndex),
+     * it is conceivable that the caller could call GetItemAtIndex(15), where 15 does not exist.
+     * GetItemAtIndex would then thrown an IndexOutOfRangeException (which extends CallerException).
+     * If the CallerException is not caught, then the Exception will be reported to the user.  The CALLER
+     * (the script who CALLED GetItemAtIndex) would have that line highlighted as being responsible
+     * for calling the error.
      *
-     * Normally, the Offset would be altered by calls to IncrementOffset
-     * at every step the CallerException is caught/rethrown up the call stack.
-     * @param string $strMessage the Message of the exception
-     * @param integer $intOffset the optional Offset value (currently defaulted to 1)
-     * @return CallerException the new exception
+     * The PHP default for exeception reporting would normally say that the "throw Exception" line in GetItemAtIndex
+     * is responsible for throwing the exception.  While this is technically true, in reality, it was the line that
+     * CALLED GetItemAtIndex which is responsible.  In short, this allows for much cleaner exception reporting.
+     *
+     * On a more in-depth note, in general, suppose a method OuterMethod takes in parameters, and ends up passing those
+     * paremeters into ANOTHER method InnerMethod which could throw a CallerException.  OuterMethod is responsible
+     * for catching and rethrowing the caller exception.  And when this is done, IncrementOffset() MUST be called on
+     * the exception object, to indicate that OuterMethod's CALLER is responsible for the exception.
+     *
+     * So the code snippet to call InnerMethod by OuterMethod should look like:
+     *    function OuterMethod($mixValue) {
+     *        try {
+     *            InnerMethod($mixValue);
+     *        } catch (CallerException $objExc) {
+     *            $objExc->IncrementOffset();
+     *            throw $objExc;
+     *        }
+     *        // Do Other Stuff
+     *    }
+     * Again, this will assure the user that the line of code responsible for the excpetion is properly being reported
+     * by the QCubed error reporting/handler.
+     * @property-read int $Offset The exception offset.
+     * @property-read string $BackTrace The exception backtrace.
+     * @property-read string $TraceArray The exception backtrace in a form of an array.
      */
-    public function __construct($strMessage, $intOffset = 1)
+    class QCallerException extends Exception
     {
-        parent::__construct($strMessage);
-        $this->intOffset = $intOffset;
-        $this->strTraceArray = debug_backtrace();
 
-        $this->file = $this->strTraceArray[$this->intOffset]['file'];
-        $this->line = $this->strTraceArray[$this->intOffset]['line'];
-    }
+        private $intOffset;
+        private $strTraceArray;
 
-    public function setMessage($strMessage)
-    {
-        $this->message = $strMessage;
-    }
+        /**
+         * The constructor of CallerExceptions.  Takes in a message string
+         * as well as an optional Offset parameter (defaults to 1).
+         * The Offset specificies how many calls up the call stack is responsible
+         * for the exception.  By definition, when a CallerException is called,
+         * at the very least the Caller of the most immediate function, which is
+         * 1 up the call stack, is responsible.  So therefore, by default, intOffset
+         * is set to 1.
+         *
+         * It is rare for intOffset to be set to an integer other than 1.
+         *
+         * Normally, the Offset would be altered by calls to IncrementOffset
+         * at every step the CallerException is caught/rethrown up the call stack.
+         * @param string $strMessage the Message of the exception
+         * @param integer $intOffset the optional Offset value (currently defaulted to 1)
+         * @return CallerException the new exception
+         */
+        public function __construct($strMessage, $intOffset = 1)
+        {
+            parent::__construct($strMessage);
+            $this->intOffset = $intOffset;
+            $this->strTraceArray = debug_backtrace();
 
-    public function IncrementOffset()
-    {
-        $this->intOffset++;
-        if (array_key_exists('file', $this->strTraceArray[$this->intOffset]))
             $this->file = $this->strTraceArray[$this->intOffset]['file'];
-        else
-            $this->file = '';
-        if (array_key_exists('line', $this->strTraceArray[$this->intOffset]))
             $this->line = $this->strTraceArray[$this->intOffset]['line'];
-        else
-            $this->line = '';
-    }
-
-    public function DecrementOffset()
-    {
-        $this->intOffset--;
-        if (array_key_exists('file', $this->strTraceArray[$this->intOffset]))
-            $this->file = $this->strTraceArray[$this->intOffset]['file'];
-        else
-            $this->file = '';
-        if (array_key_exists('line', $this->strTraceArray[$this->intOffset]))
-            $this->line = $this->strTraceArray[$this->intOffset]['line'];
-        else
-            $this->line = '';
-    }
-
-    public function __get($strName)
-    {
-        if ($strName == "Offset")
-            return $this->intOffset;
-        else if ($strName == "BackTrace") {
-            $objTraceArray = debug_backtrace();
-            return (var_export($objTraceArray, true));
-        } else if ($strName == "TraceArray") {
-            return $this->strTraceArray;
         }
-    }
 
+        public function setMessage($strMessage)
+        {
+            $this->message = $strMessage;
+        }
+
+        public function IncrementOffset()
+        {
+            $this->intOffset++;
+            if (array_key_exists('file', $this->strTraceArray[$this->intOffset]))
+                $this->file = $this->strTraceArray[$this->intOffset]['file'];
+            else
+                $this->file = '';
+            if (array_key_exists('line', $this->strTraceArray[$this->intOffset]))
+                $this->line = $this->strTraceArray[$this->intOffset]['line'];
+            else
+                $this->line = '';
+        }
+
+        public function DecrementOffset()
+        {
+            $this->intOffset--;
+            if (array_key_exists('file', $this->strTraceArray[$this->intOffset]))
+                $this->file = $this->strTraceArray[$this->intOffset]['file'];
+            else
+                $this->file = '';
+            if (array_key_exists('line', $this->strTraceArray[$this->intOffset]))
+                $this->line = $this->strTraceArray[$this->intOffset]['line'];
+            else
+                $this->line = '';
+        }
+
+        public function __get($strName)
+        {
+            if ($strName == "Offset")
+                return $this->intOffset;
+            else if ($strName == "BackTrace") {
+                $objTraceArray = debug_backtrace();
+                return (var_export($objTraceArray, true));
+            } else if ($strName == "TraceArray") {
+                return $this->strTraceArray;
+            }
+        }
+
+    }
 }
 
 class QUndefinedPrimaryKeyException extends QCallerException
